ab9ff62fda90aea964e731be5b63f57d0cc5b7b2
Nitesh Thakare <nthakare09@gmail.com>
2023-05-24 23:41:28 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularForms';
+  defaultCountry = 'india';
+  firstname: string;
+  lastname: string;
+  defaultGender = 'Female'
+
+  gender = [
+    {id: '1', value: 'Male'},
+    {id: '2', value: 'Female'},
+    {id: '3', value: 'Other'}
+  ]
+  @ViewChild('myForm') form: NgForm;
+
+  onSubmit(){
+    console.log(this.form);
+  }
+}
